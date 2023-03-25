4210fd60d513a752d89d836b3d66007c7e529347
kandavel-umapathy <kandaswamy197svg@gmail.com>
2023-03-25 21:47:33 +0530

    Initial Commit

---
+++ Emeritieus/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Emeritieus
+//
+//  Created by kandavel on 25/03/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ EmeritieusUITests/EmeritieusUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  EmeritieusUITestsLaunchTests.swift
+//  EmeritieusUITests
+//
+//  Created by kandavel on 25/03/23.
+//
+
+import XCTest
+
+class EmeritieusUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
